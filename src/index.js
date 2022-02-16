5723cf325065da1df769386f1702b21dbe4643d2
patrick <patrickjksong@gmail.com>
2022-02-15 18:44:44 -0800

    Refactored to modular js

--- src/index.js
+++ src/index.js
@@ -6,34 +6,37 @@ import './style.css';
 loadHeader();
 loadPage();
 
-const homeBtn = document.querySelector(".header :nth-child(1)");
-const menuBtn = document.querySelector(".header :nth-child(2)");
-const contactBtn = document.querySelector(".header :nth-child(3)");
+const restaurant = (() => {
+    const homeBtn = document.querySelector(".header :nth-child(1)");
+    const menuBtn = document.querySelector(".header :nth-child(2)");
+    const contactBtn = document.querySelector(".header :nth-child(3)");
 
-const add = btn => btn.classList.add("open-tab");
-const rm = btn => btn.classList.remove("open-tab");
+    const add = btn => btn.classList.add("open-tab");
+    const rm = btn => btn.classList.remove("open-tab");
 
+    homeBtn.addEventListener("click", e => {
+        const main = document.querySelector(".main");
+        if (main) content.removeChild(main);
+        loadPage();
+        rm(menuBtn);
+        rm(contactBtn);
+        add(homeBtn);
+    });
+    
+    menuBtn.addEventListener("click", e => {
+        loadMenu();
+        rm(homeBtn);
+        rm(contactBtn);
+        add(menuBtn);
+    });
+    
+    contactBtn.addEventListener("click", e => {
+        loadContact();
+        rm(homeBtn);
+        rm(menuBtn);
+        add(contactBtn);
+    });
+    
+})();
 
-homeBtn.addEventListener("click", e => {
-    const main = document.querySelector(".main");
-    if (main) content.removeChild(main);
-    loadPage();
-    rm(menuBtn);
-    rm(contactBtn);
-    add(homeBtn);
-});
-
-menuBtn.addEventListener("click", e => {
-    loadMenu();
-    rm(homeBtn);
-    rm(contactBtn);
-    add(menuBtn);
-});
-
-contactBtn.addEventListener("click", e => {
-    loadContact();
-    rm(homeBtn);
-    rm(menuBtn);
-    add(contactBtn);
-});
 
